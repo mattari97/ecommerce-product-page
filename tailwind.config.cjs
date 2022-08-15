3a9637656eeed42e749306db9582d0d372ff159f
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-14 20:53:39 -0400

    Configure challenge styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,11 +10,23 @@ module.exports = {
     colors: {
       transparent: "transparent",
       current: "currentColor",
+      primary: {
+        DEFAULT: "hsl(26, 100%, 55%)",
+        pale: "hsl(25, 100%, 94%)",
+      },
+      neutral: {
+        100: "hsl(223, 64%, 98%)",
+        200: "hsl(220, 14%, 75%)",
+        300: "hsl(219, 9%, 45%)",
+        400: "hsl(220, 13%, 13%)",
+      },
+      white: "hsl(0, 0%, 100%)",
+      black: "hsl(0, 0%, 0%)",
     },
     extend: {
-      // fontFamily: {
-      //   sans: ["Nunito\\ Sans", ...defaultTheme.fontFamily.sans],
-      // },
+      fontFamily: {
+        sans: ["Kumbh\\ Sans", ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   plugins: [],
