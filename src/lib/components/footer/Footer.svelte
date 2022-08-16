3bf5fa9f3d99385927ece650b56efc0bd9cae558
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-16 15:33:18 -0400

    Create Footer w/ attributions

---
+++ src/lib/components/footer/Footer.svelte
@@ -0,0 +1,13 @@
+<footer class="text-xs px-6 py-4 text-neutral-200 text-center ">
+  Challenge by <a
+    href="https://www.frontendmentor.io?ref=challenge"
+    target="_blank"
+    class="text-neutral-300/80 underline"
+  >
+    Frontend Mentor</a
+  >.
+  <span class="inline-block">
+    Coded by
+    <a href="https://github.com/AntoineC-dev" class="text-neutral-300/80 underline">Antoine C</a>.
+  </span>
+</footer>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import "../app.css";
   import Header from "$lib/components/header/Header.svelte";
+  import Footer from "$lib/components/footer/Footer.svelte";
 </script>
 
 <Header />
 
 <slot />
+
+<Footer />
