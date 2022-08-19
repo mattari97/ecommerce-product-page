c24a6583d6bf0ab6ac7b2fde3c665e2075903afe
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-18 21:51:56 -0400

    Add head title

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,10 @@
   import Presentation from "$lib/components/productPage/Presentation.svelte";
 </script>
 
+<svelte:head>
+  <title>Frontend Mentor | E-commerce product page</title>
+</svelte:head>
+
 <main
   class="lg:grid lg:grid-cols-2 lg:place-items-center lg:my-auto lg:py-12 lg:px-6 lg:gap-12 max-w-6xl mx-auto w-full"
 >
